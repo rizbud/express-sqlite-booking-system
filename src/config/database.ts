e4a35f808a31bf2043547714acf809925e5b0de1
Rizki Budi <rizkibudi513@gmail.com>
2023-10-15 09:55:43 +0700

    feat: setup database

---
+++ src/config/database.ts
@@ -0,0 +1,37 @@
+import Database from "better-sqlite3";
+
+const db = new Database(":memory:");
+
+export const migrateUp = () => {
+  db.exec(`CREATE TABLE IF NOT EXISTS "events" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "name" VARCHAR NOT NULL,
+    "event_date" DATE NOT NULL,
+    "capacity" INTEGER NOT NULL DEFAULT 1,
+    "available_seats" INTEGER NOT NULL DEFAULT 1,
+    "booking_started_at" DATETIME NOT NULL,
+    "booking_ended_at" DATETIME NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+  )`);
+
+  db.exec(`CREATE TABLE IF NOT EXISTS "bookings" (
+    "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+    "event_id" INTEGER NOT NULL,
+    "name" VARCHAR NOT NULL,
+    "email" VARCHAR NOT NULL UNIQUE,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY ("event_id") REFERENCES "events" ("id")
+  )`);
+
+  db.exec(
+    `CREATE INDEX IF NOT EXISTS "index_bookings_on_event_id" ON "bookings" ("event_id")`
+  );
+
+  db.exec(
+    `CREATE INDEX IF NOT EXISTS "index_capacity_on_events" ON "events" ("capacity")`
+  );
+
+  console.log("Migration completed");
+};
+
+export default db;
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
 import express from "express";
 import "dotenv/config";
 
+import { migrateUp } from "./config/database";
 import routes from "./routes";
 
 const app = express();
 const port = process.env.APP_PORT || 3000;
+
+migrateUp();
 
 app.use(express.json());
 app.use("/", routes);
 
 // error 404 handler
 app.use((req, res) => {
-  return res.status(404).json({
-    message: "Not Found",
-  });
+  return res.status(404).json({ message: "Not Found" });
 });
 
 app.listen(port, () => {
